bf0157ab39f0b7f4e5513a4b9d9aa70edb5283f3
GhostMobin <s1810677110@ru.ac.bd>
2023-11-11 02:04:33 +0600

    interactive flowbite working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import Carousel from "../components/Carousel";
 
 const Home = () => {
     return (
         <div>
+            <Carousel></Carousel>
 
         </div>
     );
